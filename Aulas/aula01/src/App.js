c185d9b5d47d83f9b93c8313dc10a8483ae25748
Alex de Matos Caceres <101301836+DeevAlex@users.noreply.github.com>
2024-11-12 00:41:04 -0300

    Add files via upload

---
+++ Aulas/aula01/src/App.js
@@ -0,0 +1,57 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+//  Componentes funcionais e componentes de classe
+
+//  componentes de classe
+
+class App extends Component { // deve ser importado o Component
+
+    render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+      );
+    }
+}
+
+
+// Componentes funcionais
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+export default App;
